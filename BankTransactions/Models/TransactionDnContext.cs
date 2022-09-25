067160ee77b37051877b4c84f95e7c0e2513d8c0
ErtugrulMehmet <108365672+ErtugrulMehmet@users.noreply.github.com>
2022-09-25 20:42:53 +0300

    Add project files.

---
+++ BankTransactions/Models/TransactionDnContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDnContext:DbContext
+    {
+        public TransactionDnContext(DbContextOptions<TransactionDnContext> options):base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
